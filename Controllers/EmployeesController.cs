034eea50b09e0b3869a7ad8c53fcb1f82e623970
Luke Cseh <lc802216@ohio.edu>
2021-03-18 17:44:19 -0400

    push change to controllers

---
+++ Controllers/EmployeesController.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using CentricProject.DAL;
+using CentricProject.Models;
+
+namespace CentricProject.Controllers
+{
+    public class EmployeesController : Controller
+    {
+        private MIS4200Context db = new MIS4200Context();
+
+        // GET: Employees
+        public ActionResult Index()
+        {
+            var employee = db.employee.Include(e => e.location).Include(e => e.position);
+            return View(employee.ToList());
+        }
+
+        // GET: Employees/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Employee employee = db.employee.Find(id);
+            if (employee == null)
+            {
+                return HttpNotFound();
+            }
+            return View(employee);
+        }
+
+        // GET: Employees/Create
+        public ActionResult Create()
+        {
+            ViewBag.locationID = new SelectList(db.location, "locationID", "location");
+            ViewBag.positionID = new SelectList(db.position, "positionID", "position");
+            return View();
+        }
+
+        // POST: Employees/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "employeeID,firstName,lastName,phone,email,positionID,locationID")] Employee employee)
+        {
+            if (ModelState.IsValid)
+            {
+                db.employee.Add(employee);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.locationID = new SelectList(db.location, "locationID", "location", employee.locationID);
+            ViewBag.positionID = new SelectList(db.position, "positionID", "position", employee.positionID);
+            return View(employee);
+        }
+
+        // GET: Employees/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Employee employee = db.employee.Find(id);
+            if (employee == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.locationID = new SelectList(db.location, "locationID", "location", employee.locationID);
+            ViewBag.positionID = new SelectList(db.position, "positionID", "position", employee.positionID);
+            return View(employee);
+        }
+
+        // POST: Employees/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "employeeID,firstName,lastName,phone,email,positionID,locationID")] Employee employee)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(employee).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.locationID = new SelectList(db.location, "locationID", "location", employee.locationID);
+            ViewBag.positionID = new SelectList(db.position, "positionID", "position", employee.positionID);
+            return View(employee);
+        }
+
+        // GET: Employees/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Employee employee = db.employee.Find(id);
+            if (employee == null)
+            {
+                return HttpNotFound();
+            }
+            return View(employee);
+        }
+
+        // POST: Employees/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Employee employee = db.employee.Find(id);
+            db.employee.Remove(employee);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ DAL/MIS4200Context.cs
@@ -0,0 +1,21 @@
+﻿using CentricProject.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace CentricProject.DAL
+{
+    public class MIS4200Context : DbContext
+    {
+        public MIS4200Context() : base("name=DefaultConnection")
+        {
+
+        }
+        public DbSet<Employee> employee { get; set; }
+        public DbSet<Location> location { get; set; }
+        public DbSet<Position> position { get; set; }
+    }
+
+}
